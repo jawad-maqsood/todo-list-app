93bdc625e9482ac1a60688bb1c2acb36cae79fa9
jawad-maqsood <jawwad.maqsoor@eonhealth.com>
2023-02-06 13:12:53 -0600

    added header and initial styling

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <div className="header d-flex justify-content-between align-items-center">
+        <span className="logo-text">TODO</span>
+        <span className="icon-moon"></span>
+      </div>
+    );
+}
+
+export default Header;
